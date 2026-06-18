adfdb4a2a13c7a8bc47d90ed6f294590313ce95b
mundongju <114848830+mundongju@users.noreply.github.com>
2026-06-18 15:56:27 +0900

    Delete run_ours.sh

--- run_ours.sh
+++
@@ -1,2 +0,0 @@
-
-python sample_condition_admm_guide_syn.py --diffusion_config configs/diffusion_config.yaml --model_config configs/imagenet_model_config.yaml --task_config configs/ll_voronoi_imagenet_ours_config.yaml --gpu 1 --save_dir ./temp_demo
